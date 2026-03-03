ce8180f7d56aa265fbce5b918caf180babd87e74
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-03-03 13:38:07 +0100

    balistyka g2 k4

--- 12_45/balistyka.py
+++ 12_45/balistyka.py
@@ -1,3 +1,7 @@
+#import math
+from math import sin, radians
+GRAVITY = 9.81
+
 while True:
     angle = float(input("angle "))
     if angle > 90 or angle < 0:
@@ -9,3 +13,7 @@ speed = float(input("speed "))
 
 print(angle)
 print(speed)
+
+z = speed**2 * sin(radians(angle * 2))/GRAVITY
+
+print(z)
