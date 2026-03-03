fdfbe5bf469a4a942597954576a36d39306036e4
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-03-03 14:31:22 +0100

    balistyka g2 k7

--- 12_45/balistyka.py
+++ 12_45/balistyka.py
@@ -1,13 +1,12 @@
 #import math
 from math import sin, radians
+from random import uniform
 GRAVITY = 9.81
 LENGTH = 80
 SCALE = 2
 
 def calculate_impact(angle, speed):
     return speed ** 2 * sin(radians(angle * 2)) / GRAVITY
-
-
 
 def get_input():
     while True:
@@ -19,23 +18,26 @@ def get_input():
 
     speed = float(input("speed "))
     return angle,speed
+
 def main():
+    player1,player2 = start_game()
     angle,speed = get_input()
     z = calculate_impact(angle, speed)
     print(z)
-    display(z)
+    display(z,player1,player2)
 
-def display(z):
+def display(z,player1,player2):
+    def scaled(pos):
+        return round(pos/SCALE)
     ground = ["_"]*LENGTH
-    ground[round(z/SCALE)] = "X"
+    ground[scaled(player1)]= "1"
+    ground[scaled(player2)]= "2"
+    ground[scaled(z)] = "X"
     [print(symbol, end = "") for symbol in ground]
-    """for symbol in ground:
-        print(symbol, end = "")"""
-    """    for i in range(LENGTH):
-        if i == round(z/SCALE):
-            print("X", end = "")
-        else:
-            print("_", end = "")"""
 
+def start_game():
+    player1 = uniform(1,LENGTH/2)*SCALE
+    player2 = uniform(LENGTH/2,LENGTH-1)*SCALE
+    return player1,player2
 if __name__ == '__main__':
     main()
