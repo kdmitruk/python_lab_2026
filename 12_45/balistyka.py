ffa034fa0dedce3eb7097f441d2a414adfa95a5b
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-03-03 13:50:57 +0100

    balistyka g2 k5

--- 12_45/balistyka.py
+++ 12_45/balistyka.py
@@ -2,18 +2,26 @@
 from math import sin, radians
 GRAVITY = 9.81
 
-while True:
-    angle = float(input("angle "))
-    if angle > 90 or angle < 0:
-        print("Improper angle")
-    else:
-        break
 
-speed = float(input("speed "))
+def calculate_impact(angle, speed):
+    return speed ** 2 * sin(radians(angle * 2)) / GRAVITY
 
-print(angle)
-print(speed)
 
-z = speed**2 * sin(radians(angle * 2))/GRAVITY
 
-print(z)
+def get_input():
+    while True:
+        angle = float(input("angle "))
+        if angle > 90 or angle < 0:
+            print("Improper angle")
+        else:
+            break
+
+    speed = float(input("speed "))
+    return angle,speed
+def main():
+    angle,speed = get_input()
+    z = calculate_impact(angle, speed)
+    print(z)
+
+if __name__ == '__main__':
+    main()
