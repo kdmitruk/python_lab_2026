17022213b12912ac75839adb9cbc759cebd9fe1f
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-03-03 11:17:00 +0100

    balistyka k5

--- 10_05/balistyka/balistyka.py
+++ 10_05/balistyka/balistyka.py
@@ -2,17 +2,33 @@
 from math import sin, radians
 
 GRAVITY = 9.81
-while True :
-    angle = float(input("Kąt: "))
-    if angle <= 0 or angle >= 90:
-        print("Wartość nie jest odpowiednia!")
-    else:
-        break
 
-velocity = float(input("Prędkość początkowa: "))
+def calculate_impact(angle, velocity):
+    return velocity**2 * sin(radians(angle)*2) / GRAVITY
 
-print(velocity)
-print(angle)
+def get_input():
+    while True :
+        angle = float(input("Kąt: "))
+        if angle <= 0 or angle >= 90:
+            print("Wartość nie jest odpowiednia!")
+        else:
+            break
 
-z = velocity**2 * sin(radians(angle)*2) / GRAVITY
-print(z)
+    velocity = float(input("Prędkość początkowa: "))
+    return angle, velocity
+
+def main():
+    while True:
+        angle, velocity = get_input()
+        print(velocity)
+        print(angle)
+
+        z = calculate_impact(angle, velocity)
+        print(z)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
