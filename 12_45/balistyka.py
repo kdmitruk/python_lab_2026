824f1e532ed352ff869e47c0384fdd5e8b3e06bd
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-03-03 13:27:07 +0100

    balistyka g2 k3

--- 12_45/balistyka.py
+++ 12_45/balistyka.py
@@ -1,6 +1,10 @@
-angle = float(input("angle "))
-if angle > 90 or angle < 0:
-    print("Improper angle")
+while True:
+    angle = float(input("angle "))
+    if angle > 90 or angle < 0:
+        print("Improper angle")
+    else:
+        break
+
 speed = float(input("speed "))
 
 print(angle)
