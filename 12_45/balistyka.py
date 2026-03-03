b00be8d99f4790cdb3e0e731229a322c36d617b8
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-03-03 14:50:02 +0100

    balistyka g2 k8a

--- 12_45/balistyka.py
+++ 12_45/balistyka.py
@@ -4,9 +4,10 @@ from random import uniform
 GRAVITY = 9.81
 LENGTH = 80
 SCALE = 2
+IMPACT_RADIUS = 2
 
-def calculate_impact(angle, speed):
-    return speed ** 2 * sin(radians(angle * 2)) / GRAVITY
+def calculate_impact(angle, speed, player):
+    return speed ** 2 * sin(radians(angle * 2)) / GRAVITY + player
 
 def get_input():
     while True:
@@ -21,19 +22,30 @@ def get_input():
 
 def main():
     player1,player2 = start_game()
-    angle,speed = get_input()
-    z = calculate_impact(angle, speed)
-    print(z)
-    display(z,player1,player2)
+    shooter = player1
+    target = player2
+    while True:
+        print(f"{"First" if shooter == player1 else "Second"} player is up:")
+        angle,speed = get_input()
+        if shooter == player2:
+            angle = 180-angle
+        z = calculate_impact(angle, speed, shooter)
+        print(z)
+        display(z,player1,player2)
+        shooter, target = target, shooter
 
 def display(z,player1,player2):
     def scaled(pos):
         return round(pos/SCALE)
     ground = ["_"]*LENGTH
-    ground[scaled(player1)]= "1"
-    ground[scaled(player2)]= "2"
-    ground[scaled(z)] = "X"
+    ground[scaled(player1)] = "1"
+    ground[scaled(player2)] = "2"
+    try:
+        ground[scaled(z)] = "X"
+    except IndexError:
+        print("Out of screen.")
     [print(symbol, end = "") for symbol in ground]
+    print()
 
 def start_game():
     player1 = uniform(1,LENGTH/2)*SCALE
