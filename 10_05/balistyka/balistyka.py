cc04be79375d23225ff7b3f19ad51139eab13a02
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-03-03 10:54:35 +0100

    balistyka k3

--- 10_05/balistyka/balistyka.py
+++ 10_05/balistyka/balistyka.py
@@ -1,6 +1,11 @@
-angle = float(input("Kąt: "))
-if not(0 >= angle >= 90):
-    print("Wartość nie jest odpowiednia!")
+
+while True :
+    angle = float(input("Kąt: "))
+    if angle <= 0 or angle >= 90:
+        print("Wartość nie jest odpowiednia!")
+    else:
+        break
+
 velocity = float(input("Prędkość początkowa: "))
 
 print(velocity)
