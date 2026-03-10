cf24620b927f79f7ae447d5c11f3b80d1085cd60
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-03-10 11:38:19 +0100

    balistyka2 g1 k4

--- 10_05/balistyka/balistyka2.py
+++ 10_05/balistyka/balistyka2.py
@@ -1,25 +1,42 @@
 import curses
 
 def main(stdscr):
-    def display(info, grid):
+    def display(info, grid,ox,oy):
         max_y, max_x = stdscr.getmaxyx()
         stdscr.erase()
-        for y in range(max_y-2):
+        view_width = max_x
+        view_height = max_y-2
+        grid_width = len(grid[0])
+        grid_height = len(grid)
+        ox = min(max(0,ox),grid_width - view_width)
+        oy = min(max(0,oy),grid_height - view_height)
+        for y in range(oy,view_height + oy):
             row = ["#" if pos else " " for pos in grid[y]]
-            row = row[:max_x]
+            row = row[ox:max_x + ox]
             row = "".join(row)
-            stdscr.addstr(y,0,row)
+            stdscr.addstr(y-oy,0,row)
         stdscr.addstr(max_y-1, 0, info)
+        return ox,oy
     stdscr.keypad(True)
     player1 = (0, 0)
     player2 = (0, 1)
     shooter = player1
     target = player2
     grid = read_file("map.txt")
+    ox = 0
+    oy = 0
     while True:
         info = f"{"First" if shooter == player1 else "Second"} player is up:"
-        display(info,grid)
+        ox,oy = display(info,grid,ox,oy)
         key = stdscr.getch()
+        if key == curses.KEY_UP:
+            oy -= 2
+        elif key == curses.KEY_DOWN:
+            oy += 2
+        elif key == curses.KEY_LEFT:
+            ox -= 4
+        elif key == curses.KEY_RIGHT:
+            ox += 4
         stdscr.refresh()
 
         shooter, target = target, shooter
