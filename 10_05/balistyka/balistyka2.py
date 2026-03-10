b18ebfa64df4e86a93a69d1b613576678d097f21
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-03-10 11:12:02 +0100

    balistyka2 g1 k3

--- 10_05/balistyka/balistyka2.py
+++ 10_05/balistyka/balistyka2.py
@@ -1,15 +1,24 @@
 import curses
 
 def main(stdscr):
+    def display(info, grid):
+        max_y, max_x = stdscr.getmaxyx()
+        stdscr.erase()
+        for y in range(max_y-2):
+            row = ["#" if pos else " " for pos in grid[y]]
+            row = row[:max_x]
+            row = "".join(row)
+            stdscr.addstr(y,0,row)
+        stdscr.addstr(max_y-1, 0, info)
     stdscr.keypad(True)
     player1 = (0, 0)
     player2 = (0, 1)
     shooter = player1
     target = player2
+    grid = read_file("map.txt")
     while True:
         info = f"{"First" if shooter == player1 else "Second"} player is up:"
-        stdscr.erase()
-        stdscr.addstr(0, 0, info)
+        display(info,grid)
         key = stdscr.getch()
         stdscr.refresh()
 
@@ -26,6 +35,6 @@ def read_file(filename):
         return grid
 
 if __name__ == '__main__':
-    read_file("map.txt")
-    # curses.wrapper(main)
+    curses.wrapper(main)
+
 
