46bf384640466d56b29b096aaf2b19b54e634660
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-03-17 13:15:59 +0100

    balistyka3 g2 k2

--- 12_45/balistyka/game.py
+++ 12_45/balistyka/game.py
@@ -1,6 +1,9 @@
+from map import Map
+
 class Game:
     def __init__(self, stdscr):
         self.stdscr = stdscr
+        map = Map("map.txt")
     def run(self):
         while True:
             key = self.stdscr.getch()
