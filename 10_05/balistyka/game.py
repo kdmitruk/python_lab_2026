6352e95711c1014e9017cf3a523e5d5dd8aa1ed4
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-03-17 10:43:15 +0100

    balistyka3 g1 k2

--- 10_05/balistyka/game.py
+++ 10_05/balistyka/game.py
@@ -1,6 +1,10 @@
+from map import Map
+
 class Game:
     def __init__(self, stdscr):
             self.stdscr = stdscr
+            self.map = Map("map.txt")
+
     def run(self):
         while True:
             pass
---
+++ 10_05/balistyka/map.py
@@ -0,0 +1,26 @@
+class Map:
+    def __init__(self, path):
+        self.grid = []
+        self.player1 = None
+        self.player2 = None
+
+    def  __load(self, path):
+        with open(path, "r") as file:
+            for y, line in enumerate(file):
+                for x, symbol in enumerate(line):
+                    if symbol == "1":
+                        self.player1 = (x, y)
+                    if symbol == "2":
+                        self.player2 = (x, y)
+
+                row = [symbol == "X" for symbol in line]
+                self.grid.append(row)
+
+    def height(self):
+        return len(self.grid)
+
+    def width(self):
+        if self.height()>0:
+            return len(self.grid[0])
+        else:
+            return 0
