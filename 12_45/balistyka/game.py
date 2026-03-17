4172c3009d58db6f8a22d837bb58563ab68e4092
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-03-17 13:38:13 +0100

    balistyka3 g2 k3

--- 12_45/balistyka/game.py
+++ 12_45/balistyka/game.py
@@ -1,9 +1,11 @@
 from map import Map
-
+from view import View
 class Game:
     def __init__(self, stdscr):
         self.stdscr = stdscr
-        map = Map("map.txt")
+        self.map = Map("map.txt")
+        self.view = View(stdscr)
     def run(self):
         while True:
+            self.view.draw(self.map)
             key = self.stdscr.getch()
---
+++ 12_45/balistyka/view.py
@@ -0,0 +1,34 @@
+class View:
+    def __init__(self,stdscr):
+        self.stdscr=stdscr
+
+    def size (self) :
+        y,x = self.stdscr.getmaxyx()
+        return x,y-2
+    def draw(self,map):
+        max_x, max_y = self.size()
+        grid_width = map.width()
+        grid_height = map.height()
+        offset_x=0
+        offset_y=0
+        offset_x = min(max(0, offset_x), grid_width - max_x)
+        offset_y = min(max(0, offset_y), grid_height - max_y)
+        self.stdscr.erase()
+        for y in range(offset_y, max_y + offset_y):
+            row = map.grid[y]
+            row = row[offset_x:max_x + offset_x]
+            row = ["#" if flag else " " for flag in row]
+            row = "".join(row)
+            self.stdscr.addstr(y - offset_y, 0, row)
+
+        self.display_player(map.player1, "1", (offset_x, offset_y), max_x, max_y)
+        self.display_player(map.player2, "2", (offset_x, offset_y), max_x, max_y)
+
+
+    def display_player(self,player,symbol,offset,view_width,view_height):
+        px,py=player
+        ox,oy=offset
+        x=px-ox
+        y=py-oy
+        if 0<=x and x<view_width and 0<=y and y<view_height:
+            self.stdscr.addch(y,x,symbol)
