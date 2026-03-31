b6b7096871ce827c1f74b59d69f21212e5614553
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-03-31 10:55:24 +0200

    pogoda g1 z4

--- 10_05/pogoda/mainwidget.py
+++ 10_05/pogoda/mainwidget.py
@@ -8,11 +8,18 @@ class MainWidget (QWidget):
         self.setWindowTitle("Pogoda")
 
         self.label = QLabel("Miasto")
+
         self.edit = QLineEdit()
+        self.edit.returnPressed.connect(self.copyText)
+
         self.button = QPushButton("Okay")
+        self.button.clicked.connect(self.copyText)
 
         self.layout = QGridLayout(self)
         self.layout.addWidget(self.label, 0, 0)
         self.layout.addWidget(self.edit, 0, 1)
         self.layout.addWidget(self.button, 1, 0, 1, 2)
 
+    def copyText(self):
+        text = self.edit.text()
+        self.label.setText(text)
