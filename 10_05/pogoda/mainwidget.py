7bae8b6343902e6a9c367b8a575e452013439e82
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-03-31 10:44:34 +0200

    pogoda g1 z3

--- 10_05/pogoda/mainwidget.py
+++ 10_05/pogoda/mainwidget.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QLineEdit, QPushButton, QLabel, QGridLayout
 
 
 class MainWidget (QWidget):
@@ -7,4 +7,11 @@ class MainWidget (QWidget):
 
         self.setWindowTitle("Pogoda")
 
+        self.edit = QLineEdit()
+        self.button = QPushButton("Okay")
+
+        self.layout = QGridLayout(self)
+        self.layout.addWidget(QLabel("Miasto"), 0, 0)
+        self.layout.addWidget(self.edit, 0, 1)
+        self.layout.addWidget(self.button, 1, 0, 1, 2)
 
