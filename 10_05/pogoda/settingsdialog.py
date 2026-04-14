9024d12ce26685463b56cee703fd1dd15c44bfbf
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-04-14 11:05:25 +0200

    pogoda g1 z13

--- 10_05/pogoda/settingsdialog.py
+++ 10_05/pogoda/settingsdialog.py
@@ -1,7 +1,24 @@
-from PySide6.QtWidgets import QDialog
+from PySide6.QtWidgets import QDialog, QCheckBox, QGridLayout, QDialogButtonBox
 
 
 class SettingsDialog(QDialog):
     def __init__(self, parent):
         super().__init__(parent)
         self.setWindowTitle("Settings")
+        self.codeBox = QCheckBox("Weather code")
+        self.temperatureBox = QCheckBox("Temperature box")
+        self.pressureBox = QCheckBox("Pressure box")
+        buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+
+        buttonBox.accepted.connect(self.accept)
+        buttonBox.rejected.connect(self.reject)
+
+        self.layout = QGridLayout(self)
+        self.layout.addWidget(self.codeBox)
+        self.layout.addWidget(self.temperatureBox)
+        self.layout.addWidget(self.pressureBox)
+        self.layout.addWidget(buttonBox)
+
+
+
+
