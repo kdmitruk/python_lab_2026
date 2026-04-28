895fd61c9458adfe019412bbbb3f3a4a8baff474
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-04-28 10:36:46 +0200

    mapa g1 z1

---
+++ 10_05/pogoda/polska.py
@@ -0,0 +1,50 @@
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+cities = {
+    "Warszawa": (52.2297, 21.0122),
+    "Kraków":   (50.0647, 19.9450),
+    "Łódź":     (51.7592, 19.4560),
+    "Wrocław":  (51.1079, 17.0385),
+    "Poznań":   (52.4064, 16.9252),
+    "Gdańsk":   (54.3520, 18.6466),
+    "Szczecin": (53.4285, 14.5528),
+    "Bydgoszcz":(53.1235, 18.0084),
+    "Lublin":   (51.2465, 22.5684),
+    "Katowice": (50.2649, 19.0238),
+    "Białystok":(53.1325, 23.1688),
+    "Rzeszów":  (50.0413, 21.9990),
+    "Olsztyn":  (53.7784, 20.4801),
+    "Kielce":   (50.8661, 20.6286),
+    "Opole":    (50.6751, 17.9213),
+    "Zielona Góra": (51.9355, 15.5062)
+}
+
+class PolandMap:
+    def __init__(self, shapefile_url=None):
+        default_url = 'https://naciscdn.org/naturalearth/50m/cultural/ne_50m_admin_0_countries.zip'
+        self.shapefile_url = shapefile_url or default_url
+        self._world = gpd.read_file(self.shapefile_url)
+        self.poland = self._world[self._world['ADMIN'] == 'Poland']
+
+    def draw(self):
+        fig, ax = plt.subplots(figsize=(8, 8))
+        self.poland.plot(ax=ax, color='lightgrey', edgecolor='black')
+        self.draw_cities(ax)
+
+    def draw_cities(self, ax):
+        xs = []
+        ys = []
+        for city in cities.values():
+            xs.append(city[1])
+            ys += [city[0]]
+        ax.scatter(xs,ys)
+
+
+
+
+
+if __name__ == '__main__':
+    poland = PolandMap()
+    poland.draw()
+    plt.show()
