bf73f2b744b3b28524e02ce77de8853371c6bc5f
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-05-05 13:14:01 +0200

    numpy g2 z1

---
+++ 12_45/tablice/tablice.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def zad1():
+    arr = np.array(
+        [[1, 2, 8, 46, 26], [110, 64, 32, 5, 0]],
+        dtype = np.uint8
+    )
+    print(arr)
+    print(arr.shape)
+
+if __name__ == '__main__':
+    zad1()
