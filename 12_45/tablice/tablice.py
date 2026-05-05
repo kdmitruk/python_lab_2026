a9a8f12117aee81ad350252c22c28c4a864ac309
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-05-05 13:49:31 +0200

    numpy g2 z3, z4

--- 12_45/tablice/tablice.py
+++ 12_45/tablice/tablice.py
@@ -1,7 +1,20 @@
 from filecmp import cmp
+from pickletools import uint8
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+def show(*imgs):
+    size = len(imgs)
+    _, axes = plt.subplots(1, size)
+    kwargs = {"cmap": 'summer', "vmin": 0, "vmax": 255}
+    if size == 1:
+        axes.imshow(imgs[0], **kwargs)
+    else:
+        for i in range(len(imgs)):
+            axes[i].imshow(imgs[i], **kwargs)
+
+    plt.show()
 
 def zad1():
     arr = np.array(
@@ -14,10 +27,18 @@ def zad1():
 def zad2():
     arr = np.zeros((100,100),dtype=np.uint8)
     arr[50,20]=255
+    return arr
 
-    plt.imshow(arr,cmap='gray')
-    plt.show()
+def zad3():
+    arr = np.random.randint(0,256,(100,100),dtype=np.uint8)
+    return arr
+
+def zad4():
+    arr = np.random.normal(128,50,(100,100))
+    return arr
 
 if __name__ == '__main__':
    # zad1()
-    zad2()
+   #  zad2()
+   #  zad3()
+   show(zad3(), zad4())
