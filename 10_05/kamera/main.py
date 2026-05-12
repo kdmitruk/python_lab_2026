4f5ccb72f2800e49891be5565b22e432dddfa62a
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-05-12 10:43:39 +0200

    kamera g1 z2

--- 10_05/kamera/main.py
+++ 10_05/kamera/main.py
@@ -1,9 +1,29 @@
 import cv2
+import matplotlib.pyplot as plt
 
 def zad1():
     img = cv2.imread("/tmp/img.webp")
-    cv2.imshow("", img)
-    cv2.waitKey()
+    plt.imshow(img)
+    plt.show()
+    #cv2.imshow("", img)
+    #cv2.waitKey()
+
+def zad2():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
 
 if __name__ == '__main__':
-    zad1()
+    zad2()
