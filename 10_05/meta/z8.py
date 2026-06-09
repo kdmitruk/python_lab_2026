81a0ae4844526fa8c08269a48d62b2dba92b49f6
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-06-09 12:10:35 +0200

    meta g1 z10

--- 10_05/meta/z8.py
+++ 10_05/meta/z8.py
@@ -1,23 +1,27 @@
 class VerboseValue:
     def __init__(self, value):
-        self.value= value
-        print("Poczatkowa wartosc: ", self.value)
+        self.value = value
+        print(f'Poczatkowa wartosc: {self.value}')
 
     def __get__(self, instance, owner):
-        print("Odczytano wartosc")
+        print(f'Zwrocono wartosc: {self.value}')
         return self.value
 
     def __set__(self, instance, value):
-        print("Ustawiono wartosc")
-        return self.value
+        self.value = value
+        print(f'Ustawiono wartosc na: {self.value}')
 
-class Test:
-    value = VerboseValue(0)
-    def __init__(self, value):
-        Test.value = VerboseValue(value)
+class Foo:
+    value = VerboseValue(5)
+
+
+def main():
+    foo = Foo()
+    foo.value = 7
+    print(foo.value)
+
+    bar = VerboseValue(3)
+    bar = 'xyz'
 
 if __name__ == '__main__':
-    test = Test("test")
-    print(test.value)
-    Test.value="asdasd"
-    print(test.value)
+    main()
---
+++ 10_05/meta/z9.py
@@ -0,0 +1,21 @@
+class Money:
+    def __init__(self):
+        self.value = 0
+
+    def __get__(self, instance, owner):
+        # return self.value
+        return round(self.value/100, 2)
+
+    def __set__(self, instance, value):
+        self.value = value*100
+
+class Wallet():
+    money = Money()
+
+def main():
+    wallet = Wallet()
+    wallet.money = 123.456
+    print(wallet.money)
+
+if __name__ == '__main__':
+    main()
